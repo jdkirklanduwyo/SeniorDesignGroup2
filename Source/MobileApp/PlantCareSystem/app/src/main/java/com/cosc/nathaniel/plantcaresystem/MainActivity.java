ded57b2bf6db025ac8f8ad1630aecccd5730b677
kuhn27 <kuhn27@gmail.com>
2017-03-02 00:56:08 -0700

    app update

--- Source/MobileApp/PlantCareSystem/app/src/main/java/com/cosc/nathaniel/plantcaresystem/MainActivity.java
+++ Source/MobileApp/PlantCareSystem/app/src/main/java/com/cosc/nathaniel/plantcaresystem/MainActivity.java
@@ -1,20 +1,28 @@
 package com.cosc.nathaniel.plantcaresystem;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     // button variables
     Button btnSettings, btnSensors, btnDatabase;
+    TextView txtRating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // instantiate textview
+        txtRating = (TextView) findViewById(R.id.textRating);
         // instantiate buttons
         btnSettings = (Button) findViewById(R.id.btnSettings);
         btnSensors = (Button) findViewById(R.id.btnSensors);
@@ -22,6 +30,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btnSettings.setOnClickListener(this);
         btnSensors.setOnClickListener(this);
         btnDatabase.setOnClickListener(this);
+
+        //set rating text
+        Double ratingNum = getRating();
+        txtRating.setText(Double.toString(ratingNum));
+        //set color of text to indicate rating
+        txtRating.setTextColor(
+                ratingNum > 7.5 ? Color.GREEN :
+                        ratingNum > 5 ? Color.YELLOW : Color.RED
+        );
+
     }
 
     @Override
@@ -36,6 +54,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
         }
         else if (v.equals(btnSettings)){
+            //test of notification display
+            createNotification();
+
             try {
                 Intent in = new Intent(v.getContext(), Settings.class);
                 startActivity(in);
@@ -56,5 +77,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private String createXML(int id, String name, int light, int water, int humid, int temp){
         return "<plant id=\"" + id + "\" name=\"" + name + "\" light=\"" + light + "\" water=\""
                 + water + "\" humid=\"" + humid + "\" temp=\"" + temp + "\"/>";
+    }
+
+    private void createNotification(){
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_light)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+                        //.setContentIntent(pendingIntent); //Required on Gingerbread and below
+        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(0, mBuilder.build());
+    }
+
+    private double getRating(){
+        //TODO get rating from server
+        return 6.2;
     }
 }
